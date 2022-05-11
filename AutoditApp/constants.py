0d0d3338804bfd484cae9eb376c8094c7d2293c1
venkatchiranjeevi <chiranjeevi@autodit.com>
2022-05-11 23:26:32 +0530

    initial set up

---
+++ AutoditApp/constants.py
@@ -0,0 +1,22 @@
+from boto3 import client as botoclient
+
+
+
+class Cognito:
+    def __init__(self):
+        pass
+    FETCH_COGNITO_TOKEN_CACHE_KEYS = "core:verify_cognito_token:{}"
+    COGNITO_CLIENT = 'cognito-idp'
+    CLIENT = botoclient(COGNITO_CLIENT,
+                        aws_access_key_id="AKIATZFC3A3LGLBZMAKV",
+                        aws_secret_access_key="Mmo3Nfa9mgY+6CFxrh+1Gqo1N8F/z+d6VDH7M7r",
+                        region_name="ap-south-1")
+
+    REFRESH_TOKEN_AUTH_FLOW = 'REFRESH_TOKEN_AUTH'
+    AUTH_TYPE = ["COGNITO", "AISATSAD"]  # if future any new auth type came then need to add here
+    CONSTANT_TRUE_VALUE = ["true", "True"]
+    CONSTANT_FALSE_VALUE = "false"
+    MOBILE_NUM_PREFIX = "+91"
+    DEFAULT_ROLE = 47
+    DEFALUT_AIRPORT = 13
+    DEFAULT_ENTITY = 9    #To avoid  errors
