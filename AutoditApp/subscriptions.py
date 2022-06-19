65bc7caeda93b1914666b7354cb104fa758c56de
Narasimha Raju <narasimharaju.seef@gmail.com>
2022-06-19 15:22:17 +0530

    Saving selected frameworks on payment success

--- AutoditApp/subscriptions.py
+++ AutoditApp/subscriptions.py
@@ -1,25 +1,41 @@
 import razorpay
 from datetime import datetime, timedelta
-from AutoditApp.models import TenantSubscriptions, SubscriptionBillingDetails
+from AutoditApp.models import TenantSubscriptions, SubscriptionBillingDetails, TenantFrameworkMaster, FrameworkMaster
 
 
 class Subscription:
     @staticmethod
     def createSubscription(data):
         plan_id = data['plan_id']
+        selected_frameworks = []
+        framework__master_pks = []
+        tenant_id = data.get("tenant_id")
+        tm = datetime.now() + timedelta(days=2)
+        for framework in data['frameworks']:
+            framework__master_pks.append(framework['id'])
+            selected_frameworks.append(
+                {"item": {"name": framework['framework_name'], "amount": 200, "currency": "INR"}})
+        subscription_data = {"plan_id": plan_id, "total_count": 2, "quantity": 1, "addons": selected_frameworks,
+                             "expire_by": int(round(tm.timestamp())), "customer_notify": 0}
+        framework_master = FrameworkMaster.objects.filter(id__in=framework__master_pks)
+        tenant_frameworks = [];
+        for framework_master_var in framework_master:
+            tenant_frameworks.append(
+                TenantFrameworkMaster(tenant_framework_name=framework_master_var.framework_name,
+                                      master_framework_id=framework_master_var.id,
+                                      framework_type=framework_master_var.framework_type,
+                                      description=framework_master_var.description,
+                                      is_deleted=0, is_active=0, tenant_id=tenant_id))
+        TenantFrameworkMaster.objects.filter(tenant_id=tenant_id).delete()
+        TenantFrameworkMaster.objects.bulk_create(tenant_frameworks)
+        print(framework_master)
         if plan_id:
             client = razorpay.Client(auth=("rzp_test_WRFdDZQHEaoM2q", "srk7PllgG016CY2DrczYYPbQ"))
             client.set_app_details({"title": "Autodit", "version": "1.0"})
-            tm = datetime.now() + timedelta(days=2)
-            selected_frameworks = []
-            for framework in data['frameworks']:
-                selected_frameworks.append({"item": {"name": framework['name'], "amount": 200, "currency": "INR"}})
-            subscription_data = {"plan_id": plan_id, "total_count": 2, "quantity": 1, "addons": selected_frameworks,
-                                 "expire_by": int(round(tm.timestamp())), "customer_notify": 0}
             try:
                 subscription_response = client.subscription.create(subscription_data)
-                expire_by = datetime.fromtimestamp(subscription_response.get("expire_by"));
-                tenant_subscription = TenantSubscriptions.objects.create(tenant_id=data.get("tenant_id"),
+                expire_by = datetime.fromtimestamp(subscription_response.get("expire_by"))
+                tenant_subscription = TenantSubscriptions.objects.create(tenant_id=tenant_id,
                                                                          subscription_id=subscription_response.get(
                                                                              "id"),
                                                                          plan_id=subscription_response.get("plan_id"),
@@ -38,6 +54,7 @@ class Subscription:
                 print(e)
                 return {"status": "FAILURE"}
         else:
+            TenantFrameworkMaster.objects.filter(tenant_id=tenant_id).update(is_active=True)
             return {"status": "SUCCESS", "payment_required": "false"}
 
     @staticmethod
@@ -57,8 +74,8 @@ class Subscription:
             tenant_subscription.status = "SUCCESS"
             tenant_subscription.payment_id = payment_id
             tenant_subscription.save()
+            TenantFrameworkMaster.objects.filter(tenant_id=tenant_id).update(is_active=True)
             return {"verified": "true"}
         except Exception as e:
             print(e)
             return {"verified": "false"}
-
