6dd2a020b69fe190e9184f01817a16cb8b17669b
AdithyaK <adithya@viewatlas.com>
2022-06-26 15:00:52 +0530

    Added permissions

--- AutoditApp/auth/views.py
+++ AutoditApp/auth/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from AutoditApp.constants import DEFAULT_VIEWS
+from AutoditApp.constants import DEFAULT_VIEWS, ADMIN_ACTIONS
 from AutoditApp.AWSCognito import Cognito
 from AutoditApp.dal import TenantMasterData, RolesData
 from AutoditApp.models import AccessPolicy, RolePolicies
@@ -59,13 +59,14 @@ class SignUp(APIView):
         tenant_data = {"tenant_name": user_name}
         tenant_obj = TenantMasterData.save_tenant_master_data(tenant_data)
         role_data = {'role_name': user_name + " ADMIN", "role_code":  user_name + "AD", "tenant_id": tenant_obj.id,
-                     "policy_name": user_name, "departments": [], "global_variables": {}, "role_type": "ADMIN", "role_for": "Admin"}
+                     "policy_name": user_name, "departments": [], "global_variables": {}, "role_type": "ADMIN",
+                     "role_for": "Admin"}
         role_obj = RolesData.save_single_role(role_data)
         new_user_data['tenant_id'] = tenant_obj.id
         new_user_data['role'] = role_obj.role_id
 
         access_policy = AccessPolicy.objects.create(policyname=user_name,
-                                                    policy={"views": DEFAULT_VIEWS, 'actions': ['*']}, type="GENERAL")
+                                                    policy={"views": DEFAULT_VIEWS, 'actions': ADMIN_ACTIONS}, type="GENERAL")
         role_policies = RolePolicies.objects.create(role_id=role_obj.role_id, accesspolicy_id=access_policy.logid)
         message, status = UsersList.add_new_user_to_cognito_userpool(new_user_data)
         if status:
